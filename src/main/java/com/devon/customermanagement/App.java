3e6a97ca1dd83fbd3094165f1e3fd609eada4864
V CHANDRA SEKHAR REDDY <68380005+vennapusac@users.noreply.github.com>
2021-03-31 10:07:46 +0530

    Service Discovery changes - Eureka Server client

--- src/main/java/com/devon/customermanagement/App.java
+++ src/main/java/com/devon/customermanagement/App.java
@@ -2,8 +2,10 @@ package com.devon.customermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class App {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(App.class);
